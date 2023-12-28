5e29709fe42e5e70fe9aa10c6079fb5bffbf1c7a
Valeriy Stroiny <155103115+ValeraStroiny@users.noreply.github.com>
2023-12-28 20:58:58 +0200

    Create class fpr devs.dart

---
+++ class fpr devs.dart
@@ -0,0 +1,23 @@
+import '../../../mocks/mock_constants.dart';
+
+    const message =
+        'ewoJImFjdGlvbiI6ICJnZXRQcm9maWxlIiwKCSJqc29uIjogIkphd2FkIiwKCSJyZXF1ZXN0X3Jlc3BvbnNlIiA6IHRydWUsCgkic2VuZGVyIjogImV4YW1wbGUiCgp9';
+
+    expect(
+        'SDKIPCMessage{action: ${Strings.GET_PROFILE}, json: $MOCK_USERNAME, sender: $MOCK_HOST, requestResponse: ${true}}',
+        sdkMessage.toString());
+  });
+
+
+    final sdkMessage = SDKIPCResponse.fromIPCMessage(message);
+    expect(Strings.GET_PROFILE, sdkMessage.action);
+    expect(false, sdkMessage.success);
+    expect(true, sdkMessage.data.isEmpty);
+    expect('somethingWentWrong', sdkMessage.errorCode);
+    expect('Wallet doesn\'t exists', sdkMessage.error);
+    expect()
+
+void main() {
+  test('string to dec', () {
+    expect(1, DecString.doubleFromDecString('100000000000000000'));
+    expect(0.1, DecString.doubleFromDecString('10000000000000000'));
